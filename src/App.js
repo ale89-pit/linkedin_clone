2d130d8005fb61d9964d05b1d1306552499b12c1
Davepuffo <123974851+Davepuffo@users.noreply.github.com>
2023-04-17 13:12:42 +0200

    ciao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import { Col, Navbar, Row } from "react-bootstrap";
-import { Footer } from "react-bootstrap/lib/Modal";
+// import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -12,20 +15,20 @@ function App() {
         <Container fluid>
           <Row>
             <Col>
-              <Navbar></Navbar>
+              <NavBar />
             </Col>
           </Row>
-          <Routes>
+          {/* <Routes>
             <Row>
               <Col>
                 <Route path="/me" element={<Profile />}></Route>
                 <Route path="/" element={<Home />}></Route>
               </Col>
             </Row>
-          </Routes>
+          </Routes> */}
           <Row>
             <Col>
-              <Footer></Footer>
+              {/* <Footer></Footer> */}
             </Col>
           </Row>
         </Container>

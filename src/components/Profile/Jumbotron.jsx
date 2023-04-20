b524bcc8392c573cb9b9cfc0ac39b4621db0ae5a
Alessio <72412290+ale89-pit@users.noreply.github.com>
2023-04-20 23:43:09 +0200

    jumbo image

--- src/components/Profile/Jumbotron.jsx
+++ src/components/Profile/Jumbotron.jsx
@@ -6,101 +6,106 @@ import { useState } from "react";
 import { Form } from "react-bootstrap";
 
 const Jumbotron = () => {
-  const user = useSelector((state) => state.profile.content.username);
+  const user = useSelector((state) => state.profile.content);
   const loading = useSelector((state) => state.profile.loading);
   let id = useSelector((state) => state.profile.content._id);
   const API_URL_PROFILE_PHOTO = `https://striveschool-api.herokuapp.com/api/profile/${id}/picture`
-  const[fd,setFd] = useState(new FormData())
-  const handleSubmit = async(e)=>{
+  const [fd, setFd] = useState(new FormData())
+  const handleSubmit = async (e) => {
     e.preventDefault()
     console.log("fetch iniziata invio foto")
     console.log(user)
     try {
-       let res = await fetch(API_URL_PROFILE_PHOTO,{
-      method:"POST",
-      body:fd,
-      headers:{
-        Authorization : "Bearer" + team.find((u)=> u.userName === user).key,
-      }
-    })
-    return res.json();
+      let res = await fetch(API_URL_PROFILE_PHOTO, {
+        method: "POST",
+        body: fd,
+        headers: {
+          Authorization: "Bearer " + team.find((u) => u.userName === user.username).key,
+        }
+      })
+      let foto = res.json();
+      console.log(foto)
     } catch (error) {
       console.log(error)
     }
-   
+
   }
-  const handleFile = (e)=>{
-    setFd((prev)=>{
+  const handleFile = (e) => {
+    setFd((prev) => {
       prev.delete("profile")
-      prev.append("profile",e.target.files[0])
+      prev.append("profile", e.target.files[0])
       return prev
     })
   }
+  console.log(user)
   return (
     <>
       {loading ? (
-        <Card className="w-100 positon-relative mb-2" id="jumbo-Card">
-          <Card.Img variant="top" id="sizeImgJumbo" src={user.image} />
+        <>
+          <Card className="w-100 positon-relative mb-2" id="jumbo-Card">
+            <Card.Img variant="top" id="sizeImgJumbo" src={user.image} alt="immagine profilo" />
+
+            <Card.Body>
+              <img
+                className="rounded-circle position-absolute imgJumboRoundend"
+                src={user.image}
+                alt="img-profile"
+              />
+              <Row>
+                <Col xs={8}>
+                  <h3 className="text-start mb-0">
+                    {user.name} {user.surname}
+                  </h3>
+                  <p className="text-start mb-1">Studente presso</p>
+                  <p className="text-start text-secondary">{user.area} </p>
+                </Col>
+                <Col xs={4}>
+                  <p className="text-end"><i className="fas fa-camera hover"></i></p>
+                  <p className="text-end">logo</p>
+                </Col>
+              </Row>
+              <Button
+                className="text-start mx-1 py-1 rounded-pill "
+                variant="primary"
+              >
+                <b>Disponibile per</b>
+              </Button>
+
+              <Button
+                className="text-start mx-1  py-1 rounded-pill"
+                variant="outline-primary"
+              >
+                Aggiungi sezione al profilo
+              </Button>
+
+              <Button
+                className="text-start mx-1 py-1 rounded-pill"
+                variant="outline-secondary"
+              >
+                <b>Altro</b>
+              </Button>
+
+              <Row className="w-100 my-3 justify-content-between">
+                <Col className="backgroudJumboDiv rounded w-50 p-2 mx-2">
+                  <p>
+                    <b>Mostra ai recruiter che sei disponibile a lavorare:</b>
+                    <br />
+                    sei tu che decidi che può vedere questa informazione. <br />
+                    <a href="#">Inizia</a>{" "}
+                  </p>
+                </Col>
+                <Col className=" backgroudJumboDiv rounded w-50 p-2 mx-2">
+                  <p>
+                    <b>Fai sapere che stai facendo selezione</b> e attrai
+                    candidati qualificati. <br /> <a href="#">Inizia</a>
+                  </p>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
           <Form onSubmit={handleSubmit}><input type="file" onChange={handleFile} />
-          <Button type="submit">invia foto</Button></Form>
-          
-          <Card.Body>
-            <img
-              className="rounded-circle position-absolute imgJumboRoundend"
-              src={user.image}
-              alt="img-profile"
-            />
-            <Row>
-              <Col xs={8}>
-                <h3 className="text-start mb-0">
-                  {user.name} {user.surname}
-                </h3>
-                <p className="text-start mb-1">Studente presso</p>
-                <p className="text-start text-secondary">{user.area} </p>
-              </Col>
-              <Col xs={4}>
-                <p className="text-end">logo</p>
-              </Col>
-            </Row>
-            <Button
-              className="text-start mx-1 py-1 rounded-pill "
-              variant="primary"
-            >
-              <b>Disponibile per</b>
-            </Button>
-
-            <Button
-              className="text-start mx-1  py-1 rounded-pill"
-              variant="outline-primary"
-            >
-              Aggiungi sezione al profilo
-            </Button>
-
-            <Button
-              className="text-start mx-1 py-1 rounded-pill"
-              variant="outline-secondary"
-            >
-              <b>Altro</b>
-            </Button>
-
-            <Row className="w-100 my-3 justify-content-between">
-              <Col className="backgroudJumboDiv rounded w-50 p-2 mx-2">
-                <p>
-                  <b>Mostra ai recruiter che sei disponibile a lavorare:</b>
-                  <br />
-                  sei tu che decidi che può vedere questa informazione. <br />
-                  <a href="#">Inizia</a>{" "}
-                </p>
-              </Col>
-              <Col className=" backgroudJumboDiv rounded w-50 p-2 mx-2">
-                <p>
-                  <b>Fai sapere che stai facendo selezione</b> e attrai
-                  candidati qualificati. <br /> <a href="#">Inizia</a>
-                </p>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
+            <Button type="submit">invia foto</Button></Form>
+        </>
       ) : (
         <div className="d-flex justify-content-center my-4">
           <Spinner></Spinner>
